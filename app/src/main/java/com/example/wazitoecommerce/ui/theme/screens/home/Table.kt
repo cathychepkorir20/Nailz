78ac33c1f00156dab10f2ea208fce7c2cc51b041
STEPHANEMURIMI <stephaneopiyo@gmail.com>
2024-05-11 09:28:16 +0300

    first commit

---
+++ app/src/main/java/com/example/wazitoecommerce/ui/theme/screens/home/Table.kt
@@ -0,0 +1,15 @@
+package com.example.wazitoecommerce.ui.theme.screens.home
+
+import android.widget.TableLayout
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Table(){
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
